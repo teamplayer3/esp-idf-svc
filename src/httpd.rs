bc742c5c197769a83ae24188ab42a8b8f11db510
Alexander Hübener <alexander.huebener@it-designers.de>
2022-10-04 10:57:57 +0200

    fixed linger

--- src/httpd.rs
+++ src/httpd.rs
@@ -5,7 +5,7 @@ use alloc::vec;
 
 use std::io;
 
-use ::anyhow::anyhow;
+use anyhow::anyhow;
 
 use log::{info, log, Level};
 
@@ -411,6 +411,10 @@ impl Server {
             open_fn: None,
             close_fn: None,
             uri_match_fn: None,
+            // Latest 4.4 and master branches have options to control SO linger,
+            // but these are not released yet so we cannot (yet) support these
+            // conditionally
+            ..Default::default()
         }
     }
 }
