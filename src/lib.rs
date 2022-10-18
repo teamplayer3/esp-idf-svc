6695cdfaa538bff9080b96646b05b1ceae815314
Alexander Hübener <alexander.huebener@it-designers.de>
2022-10-18 12:21:58 +0000

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(cfg_version)]
 #![feature(const_btree_new)]
-#![feature(generic_associated_types)] // For mutex, http, http::client, http::server, ota
 #![cfg_attr(version("1.61"), allow(deprecated_where_clause_location))]
 #![allow(unused_imports)]
 #[cfg(any(feature = "alloc"))]
